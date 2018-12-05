10b67ca5c5ce0217b6072390571e5a761e343c50
Tim Becker <tim.becker@bbva.com>
2018-12-05 10:14:46 -0600

    simplify base build for testing

--- vars/base.groovy
+++ vars/base.groovy
@@ -1,12 +1,13 @@
 def build() {
-  // Fully reset the build environment to its default clean state.
-  deleteDir()
-
-  // Pull the latest source code from the Source Control Management (SCM) system.
-  checkout scm
-
-  // Recompile all software components and durable storage migrations.
-  sh 'make build'
+  echo 'build called'
+//  // Fully reset the build environment to its default clean state.
+//  deleteDir()
+//
+//  // Pull the latest source code from the Source Control Management (SCM) system.
+//  checkout scm
+//
+//  // Recompile all software components and durable storage migrations.
+//  sh 'make build'
 }
 
 return this
